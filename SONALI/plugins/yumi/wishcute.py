2ac00557fee66a9d0bec2161968732230a0498a0
IRONMAN AND MARCO <iroboy1117@gmail.com>
2025-05-25 15:58:38 +0530

    Update wishcute.py

--- SONALI/plugins/yumi/wishcute.py
+++ SONALI/plugins/yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from SONALI import app 
 
-SUPPORT_CHAT = "https://t.me/NOBITA_MUSIC_SUPPORT"
+SUPPORT_CHAT = "https://t.me/+tVMp-_bwZVs4OWE9"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
